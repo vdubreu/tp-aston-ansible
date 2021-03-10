8066a6f2d9d813d3acb0c0c6c58a16f704b2ff36
Vincent Dubreu <Vincent@dubreu.net>
2021-03-10 15:26:03 +0000

    save3

--- filter_plugins/my_filters.py
+++ filter_plugins/my_filters.py
@@ -30,4 +30,20 @@ class FilterModule(object):
         flag = 0
         type_format = ['sawap','ext4','xfs','dos']
         line = list_device.split('\n')
-        return line
+        for i in line:
+           if 'Disk /' in i:
+              disk.append(i)
+        for v in disk:
+           inter = v.split()
+           cmd = "lsblk -f {}".format(inter[1][:-1])
+           return cmd
+           check_blk = str(subprocess.check_output(cmd,shell=True))
+           return check_blk
+         #return check_blk
+        for t in type_format:
+           if t in check_blk:
+              flag = 1
+           if flag == 0:
+              device.append(inter[1][:-1])
+           flag = 0
+        return device
